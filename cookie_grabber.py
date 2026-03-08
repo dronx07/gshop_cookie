30bfb48fc57da5702a6c601a2c9e95ea272e02d9
DRONX <dronxx07@gmail.com>
2026-03-09 02:02:16 +0800

    Update cookie_grabber.py

--- cookie_grabber.py
+++ cookie_grabber.py
@@ -70,7 +70,6 @@ def grab_session(session_id: str, keyword: str):
         "id": session_id,
         "headers": {
             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-            "Accept-Encoding": "gzip, deflate, br",
             "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8",
             "Cache-Control": "max-age=0",
             "Cookie": cookie_str,
